c141b2c7c6a8794d0163adb31085452fcb633298
Andrej Pekar <pekand@gmail.com>
2020-11-19 17:08:55 +0100

    init

---
+++ src/Log.php
@@ -0,0 +1,29 @@
+<?php
+namespace pekand\Log;
+
+class Log
+{
+    private static $storegePath = "storage/log/";
+
+    private static $allowedSeverity = ['INFO', 'ERROR', 'WARNING', 'DEBUG'];
+    
+    public static function setStoragePath($storegePath) {
+        self::$storegePath = $storegePath;
+    }
+
+    public static function setAllowdSeverity($allowedSeverity) {
+        self::$allowedSeverity = $allowedSeverity;
+    }
+    
+    public static function write($message, $severity = 'INFO') {
+        if (!in_array($severity, self::$allowedSeverity)) {
+             return;
+        }
+        
+        file_put_contents(
+            self::$storegePath."server-".date("Y-m-d").".log", 
+            date("Y-m-d H:i:s")." ".$severity." ".$message."\n", 
+            FILE_APPEND | LOCK_EX
+        );
+    }
+}
